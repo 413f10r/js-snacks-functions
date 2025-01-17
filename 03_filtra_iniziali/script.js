d3a96ee7fa8c7f661c95c2dcb368b781fce694ee
413f10r <ale-fioretti@live.it>
2025-01-17 11:41:15 +0100

    try1

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,10 +1,15 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+constarrayNomi = []
 
 // Dichiara la funzione qui.
-
+function estraiNomi(array, lettera) {
+    for (let i = 0; i < array.lenght; i++) {
+        if (array[i][0].toUpperCase() === lettera.toUpperCase())
+            arrayNomi
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,32 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
+const vocali = "aeiouAEIOU"
+let contatoreVocali = 0
+let arrayVocali = []
 
 
 // Dichiara la funzione qui.
+function letturaStringa(stringa) {
 
+    //primo ciclo analizzo ogni stringa con [i]
+    for (let i = 0; i < stringa.lenght; i++) {
+
+        //secondo ciclo per analizzare ogni vovale [k]
+        for (let k = 0; k < vocali.length; k++) {
+
+            if (stringa[i] === vocali(k)) {
+                contatoreVocali++
+                arrayVocali.push(vocali[k])
+            }
+        }
+    }
+    // return
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+letturaStringa(word)
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+console.log(contatoreVocali, arrayVocali)
